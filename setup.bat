7b1db51e93e88cbe22d81833afb56b1495e40c4b
NicholasC2 <137032388+NicholasC2@users.noreply.github.com>
2026-06-11 12:33:05 +1200

    Update setup.bat to install VS Code and Git

--- setup.bat
+++ setup.bat
@@ -1,14 +1,14 @@
 @echo off
 setlocal EnableExtensions EnableDelayedExpansion
 
+echo Installing VS Code...
+winget install -e --id Microsoft.VisualStudioCode --source winget
+
+echo Installing Git...
+winget install -e --id Git.Git --source winget
+
 echo Installing Shadowsocks...
 winget install -e --id shadowsocks.shadowsocks-windows --source winget
-
-echo Downloading Steam installer...
-winget download -e --id Valve.Steam -d "./temp" --source winget
-
-echo Running Steam installer...
-powershell -NoProfile -Command "$installer = Get-ChildItem '.\temp\Steam_*' -Recurse -Filter *.exe | Select-Object -First 1 -ExpandProperty FullName; $env:__COMPAT_LAYER='RunAsInvoker'; Start-Process $installer"
 
 echo Enabling Scripts...
 powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy Bypass -Scope CurrentUser -Force"
