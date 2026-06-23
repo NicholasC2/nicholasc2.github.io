c75f42d2688e04639b5a74b30ae6acf68777d6b8
NicholasC2 <137032388+NicholasC2@users.noreply.github.com>
2026-06-23 12:49:03 +1200

    Add Node.js installation to setup script

--- setup.ps1
+++ setup.ps1
@@ -7,5 +7,8 @@ winget install -e --id Git.Git --source winget
 Write-Host "Installing Shadowsocks..."
 winget install -e --id shadowsocks.shadowsocks-windows --source winget
 
+Write-Host "Installing Node..."
+winget install -e --id OpenJS.NodeJS --scope user --source winget
+
 Write-Host "Setting execution policy..."
 Set-ExecutionPolicy Bypass -Scope CurrentUser -Force
